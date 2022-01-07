5cca421debf12cecb733d45112edf6ec0031aa05
Peter Gakure <petergakure97@gmail.com>
2022-01-07 18:19:15 +0300

    Initial commit

--- expenses/views.py
+++ expenses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Category, Expense
+
 # Create your views here.
 from django.contrib import messages
 from django.contrib.auth.models import User
